478c99c40329bf395f8f9b601b9eb5a7306d3867
EmilyPlatzer <emilyplatzer@gmail.com>
2014-03-31 09:40:18 -0600

    User can add products to their homepage
    Products are not resubmitted upon refresh

--- product_app.rb
+++ product_app.rb
@@ -1,7 +1,15 @@
 require 'sinatra/base'
 
 class ProductApp < Sinatra::Application
+  PRODUCTS = []
   get '/' do
-    "Welcome"
+    erb :index, :locals => {:products => PRODUCTS}
+  end
+  get '/add_product' do
+    erb :add_product
+  end
+  post '/' do
+    PRODUCTS << params[:product]
+    redirect '/'
   end
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -4,8 +4,12 @@ require_relative '../product_app'
 Capybara.app = ProductApp
 
 feature "User can add products to a homepage" do
-  scenario "User is welcomed to homepage" do
+  scenario "User can add products to homepage" do
     visit '/'
+    click_link "Add a Product"
+    fill_in "product", with: "Bag of Piranhas"
+    click_on "Create Product"
     expect(page).to have_content "Welcome"
+    expect(page).to have_content "Bag of Piranhas"
   end
 end
